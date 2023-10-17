a10c75d91ee83a779603cccb1db3314415f72187
Mohammed Fayiz Ferosh <63997454+fayizferosh@users.noreply.github.com>
2023-10-17 14:59:11 +0530

    Add files via upload

---
+++ Ball/Assets/Walls.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walls : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameController.instance.hit++;
+    }
+}
